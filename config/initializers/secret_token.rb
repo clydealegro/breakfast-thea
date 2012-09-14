4f7b114be2bc16e232c4b5d2e0be0f7354934f76
Clyde Alegro <clyde.alegro@gmail.com>
2012-09-14 11:46:06 +0800

    generated breakfast-thea rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreakfastThea::Application.config.secret_token = 'c5d197a71c65fbabe55b0cc0df6c2808aa9a009cc361eecead0b8346e6b5168f7eaff12e4894af8d0e890806703aab5306a98ea561ab1ae14170a95ec4d27b5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BreakfastThea::Application.config.session_store :cookie_store, key: '_breakfast-thea_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BreakfastThea::Application.config.session_store :active_record_store
